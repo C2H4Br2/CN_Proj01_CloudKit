943b7c22945b80acb8b88882d2052ca940748a90
C2H4Br2 <trthtn0508@gmail.com>
2021-03-30 12:28:21 +0700

    another new server

--- new_Server.py
+++ new_Server.py
@@ -27,10 +27,12 @@ CEN_X = GLOBAL_W / 2; CEN_Y = GLOBAL_H / 2
 SRC = "Resources/" # 'Resources' directory
 
 # Server
+HEADER = 64 # get the length of the received message
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname()) # automatically get IP Address
 ADDR = (SERVER, PORT) # address
 FORMAT = 'utf-8' # encoding & decoding format
+DISCON_MSG = "!DISCONNECT" # disconnect message
 
 # == SUPPORTING METHODS ======================================================================
 
@@ -56,7 +58,7 @@ fnt_main = TkFont.Font(family = "Quicksand Bold", size = fnt_sz_med, weight = "n
 img_logo = PhotoImage(file = SRC + "spr_logo_terminal.png") # logo
 img_terminal = PhotoImage(file = SRC + "spr_terminal.png") # terminal
 img_btn_start = PhotoImage(file = SRC + "spr_btn_server_start.png") # start button
-img_btn_stop = PhotoImage(file = SRC + "spr_btn_server_stop.png") # start button
+img_btn_stop = PhotoImage(file = SRC + "spr_btn_server_stop.png") # shut down button
 
 # widgets
     # image labels
@@ -67,12 +69,9 @@ tb_terminal = Text(ck, width = 68, height = 16, font = fnt_main, selectbackgroun
     selectborderwidth = 0, bd = 0) # terminal
     # buttons
 btn_start = Button(ck, image = img_btn_start, borderwidth = 0, bg = COL_BG, 
-    activebackground = COL_BG, command = lambda : start_server()) # start button
+    activebackground = COL_BG, command = lambda : sv_start()) # start button
 btn_stop = Button(ck, image = img_btn_stop, borderwidth = 0, bg = COL_BG, 
-    activebackground = COL_BG, command = lambda : stop_server(), state = "disabled") # stop button
-    # others
-tlog = "" # terminal log
-
+    activebackground = COL_BG, command = lambda : sv_stop(), state = "disabled") # stop button
 # widgets positioning
 tb_terminal.configure(state = "disabled") # disable terminal
 pad_x = 36
@@ -93,57 +92,77 @@ def tm_print(msg):
     nl = "" # new line
     if tb_terminal.compare("end-1c", "!=", "1.0"): # check if terminal is not empty
         nl = "\n" # add a new line before each message
-    tb_terminal.insert(END, f"{nl}{curtime()} {msg}.") # print the message
+    tb_terminal.insert(END, f"{nl}{curtime()} {msg}") # print the message
     tb_terminal.see("end") # automatically scrolls to the end
     tb_terminal.configure(state = "disabled") # disable terminal
 
 # == SETUP: SERVER ===========================================================================
 
 server = None
-client_list = []
-
+sv_active = False # server is active or not
 
 # start server
-def start_server():
-    global server, ADDR
+def sv_start(): # server start
+    global server # enable edit on these varibles
     btn_start.config(state = "disabled") # disable start button
     btn_stop.config(state = "normal") # enable stop button
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create server socket via (Ipv4, TCP)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # allows server to reuse address
-    server.bind(ADDR) # bind the address to the socket
-    server.listen(4) # server listens for clients' connection
+    # create a socket for the server using (type: IPv4, method: TCP)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tm_print("Server started.")
+    # allow the server to reuse the address
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # bind the socket to the address
+    server.bind(ADDR)
+    tm_print("Server is bound to address.")
+    # listen for clients
+    server.listen()
+    tm_print(f"Server is listening on {SERVER}.")
+    # create a thread for sv_find_client()
+    '''
+    thread = threading.Thread(target = sv_find_client)
+    thread.start()
+    '''
 
-    # create new thread for accepting clients
-    threading._start_new_thread(accept_client, (server, " "))
+# check for client's connection
+def sv_find_client():
+    global sv_active # enable edit on these varibles
+    # activate the server
+    sv_active = True
+    while (sv_active):
+        # stop and listen & accept the client
+        conn, addr = server.accept() # (connection, address)
+        # create new thread for handling that client
+        thread = threading.Thread(target = sv_handle_client, args = (conn, addr))
+        thread.start()
 
-    # notify of the server's initialization
-    tm_print("Server online")
+# handle the clients
+def sv_handle_client(conn, addr):
+    tm_print(f"{addr} connected.")
+    
+    connected = True
+    while (connected): # wait to receive messages from the client
+        msg_len = conn.recv(HEADER).decode(FORMAT) # get the length of the message
+        msg_len = int(msg_len) # convert it to integer
+        msg = conn.recv(msg_len).decode(FORMAT)
+        if (msg == DISCON_MSG):
+            connected = False
+        
+    #close the connection
+    conn.close()
 
-# stop server
-def stop_server():
-    global server
+# shut down the server
+def sv_stop():
+    global server, sv_active # enable edit on these variables
     btn_start.config(state = "normal") # enable start button
-    btn_stop.config(state = "disabled") # disable stop button
-
-    try:
-        server.shutdown(socket.SHUT_RDWR)
-    except (socket.error, OSError, ValueError):
-        pass
+    btn_stop.config(state = "disable") # disable stop button
+    # shut down and close the server
     try:
         server.close()
+        sv_active = False
+        tm_print("Server is shut down.")
     except:
-        pass
+        tm_print("Mission failed. We'll get them next time.")
 
-    # notify of the server's termination
-    tm_print("Server shut down")
-
-# accept clients' connection
-def accept_client(t_server, t):
-    while (True):
-        client, addr = t_server.accept() # accept client's connection
-        client_list.append(client) # add client to clients list
-
-# == SETUP: SERVER-RELATED WIDGETS  ==========================================================
 
 # == MAIN PROGRAM ============================================================================
 
